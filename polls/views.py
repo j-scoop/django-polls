fdec2f81b20cb685c47d40d4b8dda495175eac66
Jack <j-bass@outlook.com>
2021-09-07 18:13:52 +0100

    Added namespacing to polls view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.template import  loader
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -9,8 +10,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    response = "You're looking at question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
@@ -24,8 +25,7 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
